0aa90b1ea512cfeefdd98de805b2ca4c29c02a17
tawmeh <tom@netelligence.com>
2021-04-16 17:58:19 -0600

    changed handler so root path will give out index.html

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.listen(8080);
 
 //handle the http request
 function handler (req, res) {
+		if (req.url == "") {
+			req.url = "/index.html";
+		}
 		fs.readFile(__dirname + req.url,  //read the index.html file and send 500 error if it fails
 		function (err, data) {
 		  if (err) {
