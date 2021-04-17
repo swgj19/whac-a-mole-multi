b7f4228f16c6922b4417cb9840d2131092d779c8
Stephen <swgj19@users.noreply.github.com>
2021-04-16 17:49:33 -0700

    updated filepath

--- server.js
+++ server.js
@@ -1,55 +1,21 @@
-var http = require('http');
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
 var fs = require('fs');
-var path = require('path');
+app.listen(8080);
 
-http.createServer(function (request, response) {
-    console.log('request ', request.url);
-
-    var filePath = '.' + request.url;
-    if (filePath == './') {
-        filePath = './index.html';
-    }
-
-    var extname = String(path.extname(filePath)).toLowerCase();
-    var mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.svg': 'image/svg+xml',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.wasm': 'application/wasm'
-    };
-
-    var contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT') {
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(404, { 'Content-Type': 'text/html' });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
-
-}).listen(8080);
-console.log('Server running at http://192.99.247.239//');
+//handle the http request
+function handler (req, res) {
+		if (req.url == "./") {
+			req.url = "./index.html";
+		}
+		fs.readFile(__dirname + req.url,  //read the index.html file and send 500 error if it fails
+		function (err, data) {
+		  if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		  }
 	  
+		  res.writeHead(200);  // send 200 response okay then send data (index.hmtl)
+		  res.end(data);
+		});
+	  }  
