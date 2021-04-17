2c9aebab7e1af1ae65fcad3d693876b0324fdfd5
Stephen <swgj19@users.noreply.github.com>
2021-04-16 17:32:10 -0700

    Added more errors and request.url change

--- server.js
+++ server.js
@@ -1,23 +1,55 @@
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
+var http = require('http');
 var fs = require('fs');
-app.listen(8080);
+var path = require('path');
 
-//handle the http request
-function handler (req, res) {
-		var filePath = '.' + request.url;
-		if (filePath == './') {
-    		filePath = './index.html';
-		}
-		fs.readFile(__dirname + req.url,  //read the index.html file and send 500 error if it fails
-		function (err, data) {
-		  if (err) {
-			res.writeHead(500);
-			return res.end('Error loading index.html');
-		  }
-	  
-		  res.writeHead(200);  // send 200 response okay then send data (index.hmtl)
-		  res.end(data);
-		});
-	  }
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './index.html';
+    }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8080);
+console.log('Server running at http://192.99.247.239//');
 	  
