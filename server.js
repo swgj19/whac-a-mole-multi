7de4ff45a4822fca670ea2014c8630ebcc7e18d7
Stephen <swgj19@users.noreply.github.com>
2021-04-18 19:00:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ function handler (req, res) {
 		if (filePath == "./") {
 			filePath = "./index.html";
 		}
-		fs.readFile(filePath,  //read the index.html file and send 500 error if it fails
+		fs.readFile(filePath,  //read index.html file and send 500 error if it fails
 		function (err, data) {
 		  if (err) {
 			res.writeHead(500);
