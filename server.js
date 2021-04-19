0aa9328c95398cfaff370115e87cdf79cbb50a4d
Stephen Willis <swgj19@yahoo.com>
2021-04-18 20:25:11 -0700

    update

--- server.js
+++ server.js
@@ -4,19 +4,21 @@ var fs = require('fs');
 app.listen(8080);
 
 //handle the http request
-function handler (req, res) {
-		var filePath = '.' + req.url;
-		if (filePath == "./") {
-			filePath = "./index.html";
-		}
-		fs.readFile(filePath,  //read index.html file and send 500 error if it fails
+function handler(req, res) {
+	var filePath = '.' + req.url;
+	if (filePath == './') {
+		filePath = './index.html';
+	}
+	fs.readFile(
+		filePath, //read index.html file and send 500 error if it fails.
 		function (err, data) {
-		  if (err) {
-			res.writeHead(500);
-			return res.end('Error loading index.html');
-		  }
-	  
-		  res.writeHead(200);  // send 200 response okay then send data (index.hmtl)
-		  res.end(data);
-		});
-	  }  
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading index.html');
+			}
+
+			res.writeHead(200); // send 200 response okay then send data (index.hmtl)
+			res.end(data);
+		}
+	);
+}
